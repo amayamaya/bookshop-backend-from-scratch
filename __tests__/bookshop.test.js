6f1012597dea16ee60f4c0d6492cc3912a368675
‘AMAYA <aamaya547@gmail.com>
2022-08-05 17:28:39 -0700

    add book passing

--- __tests__/bookshop.test.js
+++ __tests__/bookshop.test.js
@@ -32,13 +32,30 @@ describe('books routes', () => {
 
   it('/authors/:id should return data from a single author', async () => {
     const resp = await request(app).get('/authors/2');
-    console.log(resp.body);
+    // console.log(resp.body);
     expect(resp.body).toEqual({
       id: expect.any(String),
       author_name: expect.any(String),
       author_dob: expect.any(String),
       author_pob_pop: expect.any(String),
       books: expect.any(Array),
+    });
+  });
+
+  it('/books should create a new book', async () => {
+    const resp = await request(app)
+      .post('/books')
+      .send({
+        title: 'Something Different',
+        released: '2222',
+        authors: ['1', '2'],
+      });
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      title: expect.any(String),
+      released: expect.any(String),
+      authors: expect.any(Array),
     });
   });
 
